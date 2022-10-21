f7a97aca8b7d70dc5c9d025fb2184b4cce2b9631
robinvolf <xvolfr00@stud.fit.vutbr.cz>
2022-10-21 20:22:54 +0200

    Implementace infrastruktury pro praci s tokeny

--- src/scanner.c
+++ src/scanner.c
@@ -1,7 +1,6 @@
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
-#include <stdio.h>
+#include "scanner.h"
+
+const char *open_sign = "<?php";
 
 /* Funkce pro overeni, ze soubor STREAM na zacatku obsahuje znaky SIGN
  * stream = odtud nacita znaky
@@ -20,4 +19,33 @@ bool sign_check(FILE *stream, const char *sign) {
     } else {
         return false;
     }
+}
+
+/* Funkce pro praci s tokeny*/
+
+/* Funkce pro naplneni tokenu - argumenty funkce budou jednotlive polozky tokenu
+ * Pro zvyseni citelnosti kodu pri tvorbe tokenu
+ * content = obsah tokenu (nactene symboly)
+ * token_var = varianta tokenu
+ * linu_num = cislo radku, na kterem byl token nacteny
+ * */
+void fill_token(token_t *token, char* content, token_var variant, int line_num) {
+    token->content = content;
+    token->variant = variant;
+    token->line_num = line_num;
+}
+
+/* Funkce pro vypsani tokenu, pro debugovaci ucely
+ * token = ukazatel na token, ktery se ma vypsat
+ * */
+void print_token(token_t *token) {
+    printf("Token: \"%s\" Variant: %d Line: %d\n", token->content, token->variant, token->line_num); 
+}
+
+/* Funkce pro uvolneni pameti na heapu zabirane tokenem
+ * token = ukazatel na token, ktery se ma  uvolnit
+ * */
+void free_token(token_t *token) {
+    free(token->content);
+    token->content = NULL;
 }
--- src/scanner.h
+++ src/scanner.h
@@ -1,13 +1,36 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
-#include <stdio.h>
 
 #ifndef SCANNER_H_
-#define SCANNER_H_
+#define SCANNER_H_ 1
 
-const char *open_sign = "<?php";
+/* Enum pro urceni stavu automatu ridiciho lexikalni analyzu
+ * */
+typedef enum {default_s, div_oper_s, com_oneline} scanner_state; //TODO
+
+/* Enum pro urceni varianty tokenu (je to operator '/' nebo identifikator nebo ...)
+ * */
+typedef enum {div_oper} token_var; //TODO
+
+/* Struktura predstavujici token
+ * content = obsah tokenu (nactene symboly)
+ * token_var = varianta tokenu, je to enum deklarovany vyse
+ * line_num = cislo radku, na kterem byl token nacteny
+ * */
+typedef struct token_struct{
+    char *content;
+    token_var variant;
+    int line_num;
+} token_t;
 
 bool sign_check(FILE *stream, const char *sign);
+
+void fill_token(token_t* token, char* content, token_var variant, int line_num);
+
+void print_token(token_t *token);
+
+void free_token(token_t *token);
 
 #endif
