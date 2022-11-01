9a21ac57fe54a1891b40607f063d57b8544b3a8c
robinvolf <xvolfr00@stud.fit.vutbr.cz>
2022-11-01 09:10:30 +0100

    Zvysena citelnost vystupu testovaciho skriptu

--- src/test-skript.sh
+++ src/test-skript.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/bash
 
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
+
 cd tests/ && for TEST in *; do
-diff --color -u <(tail -n +3 $TEST) <(../scanner-test < <(head -n 1 $TEST))
+    echo -n "${BOLD}${TEST}${NORMAL}"
+    diff --color -u <(tail -n +3 $TEST) <(../scanner-test < <(head -n 1 $TEST))
+    if [ $? == 0 ]; then
+        echo "${BOLD} - SUCCESS${NORMAL}"
+    fi
 done
     
