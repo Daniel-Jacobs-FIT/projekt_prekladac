8b4ab325efdca411d5ac3c21a4ff3ef03e3c0a47
robinvolf <xvolfr00@stud.fit.vutbr.cz>
2022-11-01 14:04:43 +0100

    Uprava zadani testovaciho skriptu, rozdeleni testu do in a ref slozek

--- src/test-skript.sh
+++ src/test-skript.sh
@@ -3,11 +3,11 @@
 BOLD=$(tput bold)
 NORMAL=$(tput sgr0)
 
-cd tests/ && for TEST in *; do
-    echo -n "${BOLD}${TEST}${NORMAL}"
-    diff --color -u <(tail -n +3 $TEST) <(../scanner-test < <(head -n 1 $TEST))
+cd tests-in/ && for TEST in *; do
+    echo "${BOLD}${TEST}:${NORMAL}"
+    diff --color -u ../tests-ref/${TEST}-ref <(../scanner-test < $TEST)
     if [ $? == 0 ]; then
-        echo "${BOLD} - SUCCESS${NORMAL}"
+        printf "\U21b3 ${BOLD}SUCCESS${NORMAL}\n\n"
     fi
 done
     
