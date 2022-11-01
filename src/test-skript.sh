475c3f964a8b404439f3a62d9965ab7e0fec0b65
robinvolf <xvolfr00@stud.fit.vutbr.cz>
2022-11-01 08:34:05 +0100

    Oprava chyby v testovacim skriptu, oprava zpracovani bilych znaku v default_logic()

--- src/test-skript.sh
+++ src/test-skript.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 cd tests/ && for TEST in *; do
-diff --color -u <(tail -n +3 $TEST) <(../scanner-test <(head -n 1 $TEST))
+diff --color -u <(tail -n +3 $TEST) <(../scanner-test < <(head -n 1 $TEST))
 done
     
