d78b449116c4265124918fd882c41540b3c7d764
robinvolf <xvolfr00@stud.fit.vutbr.cz>
2022-11-15 14:13:48 +0100

    Pridani moznosti oznacit test, aby se neprovadel

--- src/test-skript.sh
+++ src/test-skript.sh
@@ -9,15 +9,17 @@ SCTEST=tests/scanner
 cd ${SCTEST} && for DIR in *; do
     cd  ${DIR} && for TEST in *; do
         if [ -f ${TEST} ]; then
-            echo "${BOLD}${TEST}:${NORMAL}"
-            if [ -f refs/${TEST}-ref ]; then
-                diff --color -u refs/${TEST}-ref <(../../../scanner-test < $TEST)
-                if [ $? == 0 ]; then
-                    printf "\U21b3 ${BOLD}${GREEN}SUCCESS${NORMAL}\n\n"
+            if ! [[ ${TEST} =~ ^@ ]]; then
+                echo "${BOLD}${TEST}:${NORMAL}"
+                if [ -f refs/${TEST}-ref ]; then
+                    diff --color -u refs/${TEST}-ref <(../../../scanner-test < $TEST)
+                    if [ $? == 0 ]; then
+                        printf "\U21b3 ${BOLD}${GREEN}SUCCESS${NORMAL}\n\n"
+                    fi
+                else
+                    echo "${BOLD}No Refs Found! Printing STDOUT${NORMAL}"
+                    echo "$( ../../../scanner-test < $TEST )"
                 fi
-            else
-                echo "${BOLD}No Refs Found! Printing STDOUT${NORMAL}"
-                echo "$( ../../../scanner-test < $TEST )"
             fi
         fi
     done
